510e6d27a991a4404db3ffc6520cea45ca87ffa2
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-27 15:42:25 +0200

    first commit

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        char[] s = {'h','e','l','l','o'};
+        for (int i = 0; i < s.length/2; i++){
+            char tmp = s[i];
+            s[i] = s[s.length-i-1];
+            s[s.length-i-1] = tmp;
+        }
+
+        System.out.println(Arrays.toString(s));
+
+    }
+}
