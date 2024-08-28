cbfa00ffbe06775408738fddd7f1528de4817f12
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-08-28 09:30:37 +0200

    update code

--- src/ReverseIntegerOpti.java
+++ src/ReverseIntegerOpti.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ReverseIntegerOpti {
+public class ReverseIntegerOpti {
+
+    public static void main(String[] args) {
+        int x = 1234;
+        long reversed = 0;
+        while (x != 0) {
+            reversed = reversed * 10 + x%10;
+            x/=10;
+            if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE){
+                reversed = 0;
+            }
+        }
+        System.out.println(reversed);
+    }
 }
