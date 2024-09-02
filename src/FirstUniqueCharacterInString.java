706db7f62017e2d2560a8716ea45a12c17d28243
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-02 11:41:28 +0200

    update code

---
+++ src/FirstUniqueCharacterInString.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FirstUniqueCharacterInString {
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        checkFirstUniqueCharacter(s);
+        System.out.println(checkFirstUniqueCharacter(s));
+    }
+
+    private static Integer checkFirstUniqueCharacter(String s) {
+        String[] stringArray = s.split("");
+        Map<String, Integer> counterMap = new HashMap<>();
+
+        // Count occurrences of each character
+        for (String theCharacter : stringArray) {
+            counterMap.put(theCharacter, counterMap.getOrDefault(theCharacter, 0) + 1);
+        }
+
+        // Find the first unique character
+        for (int i = 0; i < s.length(); i++) {
+            String letter = stringArray[i];
+            if (counterMap.get(letter) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
