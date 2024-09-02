018d7f88bc4271c7d6e649289f2cc96e89029043
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-02 12:29:42 +0200

    update code

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,21 @@
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("kawak"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        boolean isPalindrome = true;
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (arr[i] != arr[n - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
+    }
+}
