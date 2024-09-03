8e367f41530634560801fee6a237fc05e81ee303
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-03 11:06:53 +0200

    update code

---
+++ src/SingleNumber.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        int singleNumber = findSingleNumber(nums);
+        System.out.println(singleNumber);  // Output: 4
+    }
+
+    private static int findSingleNumber(int[] nums) {
+        int result = 0;
+        for (int number : nums) {
+            result ^= number;  // XOR each number
+        }
+        return result;  // The remaining number is the unique number
+    }
+}
